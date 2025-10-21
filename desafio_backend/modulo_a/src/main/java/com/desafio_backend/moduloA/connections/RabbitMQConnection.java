3854ed1f7695c951ec51d9dc7195ab53cc447f50
BernardoMendess <bernardo_mendes@id.uff.br>
2025-10-21 16:11:50 -0300

    chore(rabbitmq): inicializa configuracao de mensageria
    adiciona a configuracao base para a comunicacao com o RabbitMQ

---
+++ desafio_backend/modulo_a/src/main/java/com/desafio_backend/moduloA/connections/RabbitMQConnection.java
@@ -0,0 +1,51 @@
+package com.desafio_backend.moduloA.connections;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitMQConnection {
+    private static final String NOME_EXCHANGE = "amq.direct";
+
+    private AmqpAdmin amqpAdmin;
+
+    public RabbitMQConnection(AmqpAdmin amqpAdmin){
+        this.amqpAdmin = amqpAdmin;
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    private Queue fila(String nomeFila) {
+        return new Queue(nomeFila, true, false, false);
+    }
+
+    private DirectExchange trocaDireta() {
+        return new DirectExchange(NOME_EXCHANGE);
+    }
+
+    private Binding relacionamento(Queue fila, DirectExchange troca) {
+        return new Binding(fila.getName(), Binding.DestinationType.QUEUE, troca.getName(), fila.getName(), null);
+    }
+
+    @PostConstruct
+    private void adicionaFila() {
+        Queue fila = fila("pedidos.recebidos");
+        DirectExchange troca = trocaDireta();
+
+        Binding ligacao = relacionamento(fila, troca);
+
+        amqpAdmin.declareQueue(fila);
+        amqpAdmin.declareExchange(troca);
+        amqpAdmin.declareBinding(ligacao);
+    }
+}
---
+++ desafio_backend/modulo_a/src/main/java/com/desafio_backend/moduloA/service/RabbitMQService.java
@@ -0,0 +1,17 @@
+package com.desafio_backend.moduloA.service;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQService {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public void enviaMensagem(String nomeFila, Object mensagem) {
+        rabbitTemplate.convertAndSend(nomeFila, mensagem);
+    }
+
+}
