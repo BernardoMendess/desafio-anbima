348636372012c4e7ff4238e9b03d7d0a301e97ff
BernardoMendess <bernardo_mendes@id.uff.br>
2025-10-21 03:23:12 -0300

    feat: commit inicial do projeto

---
+++ desafio_backend/modulo_a/src/test/java/com/desafio_backend/moduloA/ModuloAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio_backend.moduloA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
