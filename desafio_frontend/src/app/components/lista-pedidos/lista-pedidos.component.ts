ae691f7245ded4a55ea432960ea6b1c16f86cdc8
BernardoMendess <bernardo_mendes@id.uff.br>
2025-10-23 04:46:26 -0300

    feat(pedido): implementa botao de atualizar

--- desafio_frontend/src/app/components/lista-pedidos/lista-pedidos.component.ts
+++ desafio_frontend/src/app/components/lista-pedidos/lista-pedidos.component.ts
@@ -38,6 +38,11 @@ export class ListaPedidosComponent implements OnInit {
     });
   }
 
+  pesquisarSemFiltros() {
+    this.statusFiltro = '';
+    this.pesquisar();
+  }
+
   novoPedido() {
     this.router.navigate(['/pedidos/novo']);
   }
